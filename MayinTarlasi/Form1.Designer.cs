ee21c60c93adb62399f84d3467115f2fd19c5b5a
Osman Erdem Kılıç <osman_erdem1200@hotmail.com>
2021-07-22 15:16:37 +0300

    Add project files.

---
+++ MayinTarlasi/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace MayinTarlasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uretbutton = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.skor = new System.Windows.Forms.Label();
+            this.mayinsayisi = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // uretbutton
+            // 
+            this.uretbutton.Location = new System.Drawing.Point(625, 12);
+            this.uretbutton.Name = "uretbutton";
+            this.uretbutton.Size = new System.Drawing.Size(163, 145);
+            this.uretbutton.TabIndex = 0;
+            this.uretbutton.Text = "Üret";
+            this.uretbutton.UseVisualStyleBackColor = true;
+            this.uretbutton.Click += new System.EventHandler(this.uretbutton_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(607, 426);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // skor
+            // 
+            this.skor.AutoSize = true;
+            this.skor.Location = new System.Drawing.Point(636, 176);
+            this.skor.Name = "skor";
+            this.skor.Size = new System.Drawing.Size(41, 17);
+            this.skor.TabIndex = 1;
+            this.skor.Text = "Skor:";
+            // 
+            // mayinsayisi
+            // 
+            this.mayinsayisi.AutoSize = true;
+            this.mayinsayisi.Location = new System.Drawing.Point(639, 206);
+            this.mayinsayisi.Name = "mayinsayisi";
+            this.mayinsayisi.Size = new System.Drawing.Size(49, 17);
+            this.mayinsayisi.TabIndex = 2;
+            this.mayinsayisi.Text = "Mayın:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mayinsayisi);
+            this.Controls.Add(this.skor);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.uretbutton);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mayın Tarlası";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uretbutton;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label skor;
+        private System.Windows.Forms.Label mayinsayisi;
+    }
+}
+
---
+++ MayinTarlasi/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MayinTarlasi
+{
+    public partial class Form1 : Form
+    {
+        int puan=0;
+        int mayinsayi=5;
+        int mayinsayac = 5;
+
+        Button btnOrnek;
+        public Form1()
+        {
+            InitializeComponent();
+            skor.Text = "Skor: " + puan;
+            mayinsayisi.Text = "Mayın: " + mayinsayi;
+        }
+
+        private void uretbutton_Click(object sender, EventArgs e)
+        {
+          
+            int mayin1 = 0;
+            int mayin2 = 0;
+            int mayin3 = 0;
+            int mayin4 = 0;
+            int mayin5 = 0;
+
+            Random random = new Random();
+            mayin1 = random.Next(1, 20);
+            mayin2 = random.Next(21, 40);
+            mayin3 = random.Next(41, 60);
+            mayin4 = random.Next(61, 80);
+            mayin5 = random.Next(81, 100);
+
+            for (int i = 1; i <= 100; i++)
+            {
+                btnOrnek = new Button();
+
+                btnOrnek.Name = "btn" + i.ToString();
+                btnOrnek.Size= new System.Drawing.Size(32, 32);
+                btnOrnek.Text = i.ToString();
+                if (mayin1==i || mayin2==i || mayin3==i || mayin4==i || mayin5==i )
+                {
+                    btnOrnek.Tag = true;
+                }
+                else
+                {
+                    btnOrnek.Tag = false;
+                }
+            
+                btnOrnek.UseVisualStyleBackColor = true;
+                btnOrnek.Click += btnOrnek_Click;
+                flowLayoutPanel1.Controls.Add(btnOrnek);
+
+
+                if (mayinsayac==0)
+                {
+                    btnOrnek.Enabled = false;
+                }
+
+
+                //this.btnOrnek.Location = new System.Drawing.Point(3, 3);
+                //this.btnOrnek.Name = "btnOrnek";
+                //this.btnOrnek.Size = new System.Drawing.Size(32, 32);
+                //this.btnOrnek.TabIndex = 0;
+                //this.btnOrnek.Text = "1";
+                //this.btnOrnek.UseVisualStyleBackColor = true;
+
+            }
+
+           
+        }
+
+        private void btnOrnek_Click(object sender, EventArgs e)
+        {
+            
+            Button basilanbutton = ((Button)sender);
+            bool mayinbulundumu = (bool)basilanbutton.Tag;
+
+            if (mayinbulundumu)
+            {
+                puan = puan - 10;
+                mayinsayi-=1;
+                MessageBox.Show("Mayına bastınız.");
+                basilanbutton.BackColor = Color.Red;
+                skor.Text = "Skor: " + puan;
+                mayinsayisi.Text = "Mayın: " + mayinsayi;
+                mayinsayac--;
+                basilanbutton.Enabled = false;
+                if (mayinsayac==0)
+                {
+                   DialogResult result= MessageBox.Show("Mayınların Hepsine Bastınız!!", "OYUN BİTTİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    uretbutton.Enabled = false;
+                     
+                     
+                    
+                }
+            }
+            else
+            {
+                puan = puan + 10;               
+                basilanbutton.BackColor = Color.Green;
+                skor.Text = "Skor: " + puan;
+                mayinsayisi.Text = "Mayın: " + mayinsayi;
+                basilanbutton.Enabled = false;
+            }
+         
+        }
+    }
+}
